c85c13ed378d2b3032f2268da942c1011c471e3c
Sebastian Saenz <sebastian.saenz1203@gmail.com>
2024-03-19 21:07:45 -0400

    worked on homeRoutes.js and the index.js , also created the api routes for comments, posts and user

---
+++ controllers/index.js
@@ -0,0 +1,9 @@
+// only importing the necessary routes
+import { Router } from 'express'
+import { homeRoutes } from './homeRoutes.js'
+import { apiIndex } from './api/index.js'
+
+export const routes = Router();
+
+routes.use('/', homeRoutes);
+routes.use('/api', apiIndex);
