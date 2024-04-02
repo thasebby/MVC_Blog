672ba41616e1b3aed3b96df6dab731f45169a7f4
Sebastian Saenz <sebastian.saenz1203@gmail.com>
2024-04-02 16:45:29 -0400

    small typo still not working

--- public/js/signUp.js
+++ public/js/signUp.js
@@ -1,37 +1,46 @@
-import $ from './utils/jQuery.js'
+// import $ from './utils/jQuery.js'
 
-let signUpForm = $('#signupBtn')
+$(document).ready(function () {
+    let signUpForm = $('#signupBtn')
 
-const signUpFormHandler = async (event) => {
-    event.preventDefault()
+    const signUpFormHandler = async (event) => {
+        event.preventDefault()
 
-    // fetch username,password and email
-    const username = $('#usernameSignup').val().trim()
-    const email = $('#emailSignup').val().trim()
-    const password = $('#passwordSignup').val().trim()
+        // fetch username,password and email
+        const username = $('#usernameSignup').val().trim()
+        const email = $('#emailSignup').val().trim()
+        const password = $('#passwordSignup').val().trim()
+        console.log(username, email, password)
+        if (username && email && password) {
+            // consolidating info 
+            let newUser = {
+                username: username,
+                email: email,
+                password: password,
+            }
+            try {
+                const response = await fetch('/api/users/signup', {
+                    method: 'POST',
+                    body: JSON.stringify({ newUser }),
+                    headers: { 'Content-Type': 'application/json' },
+                });
 
-    if(username && email && password){
-        // consolidating info 
-        let newUser = {
-            username: username,
-            email: email,
-            password: password,
+                if (response.ok) {
+                    const responseData=await response.json();
+                    console.log('Signup Successful:', responseData);
+                    // console.log(response.ok)
+                    // document.location.replace('/');
+                }
+                else {
+                    console.error('Signup request failed:',response.statusText)
+                }
+            } catch (error) {
+                console.error('Network error:, error');
+            }
         }
+    };
 
-        const response = await fetch ('/api/users/signup', {
-            method: 'POST',
-            body: JSON.stringify({newUser}),
-            headers: { 'Content-Type': 'application/json'},
-        });
-
-        if(response.ok) {
-            document.location.replace('/');
-        }
-        else{
-            console.log(response.statusText)
-        }
-    }
-};
-
-// event listener
-signUpForm.submit(signUpFormHandler)
+    // event listener
+    // signUpForm.click(signUpFormHandler)
+    signUpForm.on('click', signUpFormHandler)
+});
